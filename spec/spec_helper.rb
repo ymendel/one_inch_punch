ed14d652cffdeb1559f04c4cae2f7a3ebd6290d2
Yossef Mendelssohn <ymendel@pobox.com>
2008-08-17 14:05:07 -0500

    Using mocha for mocking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,18 @@ rescue LoadError
   require 'spec'
 end
 
+# this is my favorite way to require ever
+begin
+  require 'mocha'
+rescue LoadError
+  require 'rubygems'
+  gem 'mocha'
+  require 'mocha'
+end
+
+Spec::Runner.configure do |config|
+  config.mock_with :mocha
+end
+
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'one_inch_punch'
