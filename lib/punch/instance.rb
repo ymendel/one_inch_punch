6f7c8c7fa2ae9ea5c22d1ddb477ef3cb7eee1276
Yossef Mendelssohn <ymendel@pobox.com>
2008-11-05 22:55:15 -0600

    Querying status on a Punch instance.

--- lib/punch/instance.rb
+++ lib/punch/instance.rb
@@ -4,4 +4,16 @@ class Punch
   def initialize(project)
     @project = project
   end
+  
+  def status
+    self.class.status(project)
+  end
+  
+  def out?
+    self.class.out?(project)
+  end
+  
+  def in?
+    self.class.in?(project)
+  end
 end
--- spec/punch_instance_spec.rb
+++ spec/punch_instance_spec.rb
@@ -1,11 +1,12 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Punch, 'instance' do
+  before :each do
+    @project = 'proj'
+    @punch = Punch.new(@project)
+  end
+  
   describe 'when initialized' do
-    before :each do
-      @project = 'proj'
-    end
-    
     it 'should accept a project' do
       lambda { Punch.new(@project) }.should_not raise_error(ArgumentError)
     end
@@ -16,6 +17,81 @@ describe Punch, 'instance' do
     
     it 'should save the project for later use' do
       Punch.new(@project).project.should == @project
+    end
+  end
+  
+  it 'should give project status' do
+    @punch.should respond_to(:status)
+  end
+  
+  describe 'giving project status' do
+    before :each do
+      @status = 'status val'
+      Punch.stubs(:status).returns(@status)
+    end
+    
+    it 'should delegate to the class' do
+      Punch.expects(:status)
+      @punch.status
+    end
+    
+    it 'should pass the project when delegating to the class' do
+      Punch.expects(:status).with(@project)
+      @punch.status
+    end
+    
+    it 'should return the value returned by the class method' do
+      @punch.status.should == @status
+    end
+  end
+  
+  it 'should indicate whether the project is punched out' do
+    @punch.should respond_to(:out?)
+  end
+  
+  describe 'indicating whether the project is punched out' do
+    before :each do
+      @out = 'out val'
+      Punch.stubs(:out?).returns(@out)
+    end
+    
+    it 'should delegate to the class' do
+      Punch.expects(:out?)
+      @punch.out?
+    end
+    
+    it 'should pass the project when delegating to the class' do
+      Punch.expects(:out?).with(@project)
+      @punch.out?
+    end
+    
+    it 'should return the value returned by the class method' do
+      @punch.out?.should == @out
+    end
+  end
+  
+  it 'should indicate whether the project is punched in' do
+    @punch.should respond_to(:in?)
+  end
+  
+  describe 'indicating whether the project is punched in' do
+    before :each do
+      @in = 'in val'
+      Punch.stubs(:in?).returns(@in)
+    end
+    
+    it 'should delegate to the class' do
+      Punch.expects(:in?)
+      @punch.in?
+    end
+    
+    it 'should pass the project when delegating to the class' do
+      Punch.expects(:in?).with(@project)
+      @punch.in?
+    end
+    
+    it 'should return the value returned by the class method' do
+      @punch.in?.should == @in
     end
   end
 end
