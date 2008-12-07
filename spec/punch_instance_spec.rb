30868b5a74530eaad354eee1267b7ae6f1c0a2b3
Yossef Mendelssohn <ymendel@pobox.com>
2008-12-07 16:26:51 -0600

    Changing punch instance spec for bacon/facon.
    Once again, having to expand some specifications because of facon's lack of expectation matchers.

--- spec/punch_instance_spec.rb
+++ spec/punch_instance_spec.rb
@@ -1,18 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Punch, 'instance' do
-  before :each do
+  before do
     @project = 'proj'
     @punch = Punch.new(@project)
   end
   
   describe 'when initialized' do
     it 'should accept a project' do
-      lambda { Punch.new(@project) }.should_not raise_error(ArgumentError)
+      lambda { Punch.new(@project) }.should.not.raise(ArgumentError)
     end
     
     it 'should require a project' do
-      lambda { Punch.new }.should raise_error(ArgumentError)
+      lambda { Punch.new }.should.raise(ArgumentError)
     end
     
     it 'should save the project for later use' do
@@ -21,22 +21,22 @@ describe Punch, 'instance' do
   end
   
   it 'should give project status' do
-    @punch.should respond_to(:status)
+    @punch.should.respond_to(:status)
   end
   
   describe 'giving project status' do
-    before :each do
+    before do
       @status = 'status val'
-      Punch.stubs(:status).returns(@status)
+      Punch.stub!(:status).and_return(@status)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:status)
+      Punch.should.receive(:status)
       @punch.status
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:status).with(@project)
+      Punch.should.receive(:status).with(@project)
       @punch.status
     end
     
@@ -46,22 +46,22 @@ describe Punch, 'instance' do
   end
   
   it 'should indicate whether the project is punched out' do
-    @punch.should respond_to(:out?)
+    @punch.should.respond_to(:out?)
   end
   
   describe 'indicating whether the project is punched out' do
-    before :each do
+    before do
       @out = 'out val'
-      Punch.stubs(:out?).returns(@out)
+      Punch.stub!(:out?).and_return(@out)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:out?)
+      Punch.should.receive(:out?)
       @punch.out?
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:out?).with(@project)
+      Punch.should.receive(:out?).with(@project)
       @punch.out?
     end
     
@@ -71,22 +71,22 @@ describe Punch, 'instance' do
   end
   
   it 'should indicate whether the project is punched in' do
-    @punch.should respond_to(:in?)
+    @punch.should.respond_to(:in?)
   end
   
   describe 'indicating whether the project is punched in' do
-    before :each do
+    before do
       @in = 'in val'
-      Punch.stubs(:in?).returns(@in)
+      Punch.stub!(:in?).and_return(@in)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:in?)
+      Punch.should.receive(:in?)
       @punch.in?
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:in?).with(@project)
+      Punch.should.receive(:in?).with(@project)
       @punch.in?
     end
     
@@ -96,41 +96,47 @@ describe Punch, 'instance' do
   end
   
   it 'should punch the project in' do
-    @punch.should respond_to(:in)
+    @punch.should.respond_to(:in)
   end
   
   describe 'punching the project in' do
-    before :each do
+    before do
       @in = 'in val'
-      Punch.stubs(:in).returns(@in)
+      Punch.stub!(:in).and_return(@in)
     end
     
     it 'should accept options' do
-      lambda { @punch.in(:time => Time.now) }.should_not raise_error(ArgumentError)
+      lambda { @punch.in(:time => Time.now) }.should.not.raise(ArgumentError)
     end
     
     it 'should not require options' do
-      lambda { @punch.in }.should_not raise_error(ArgumentError)
+      lambda { @punch.in }.should.not.raise(ArgumentError)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:in)
+      Punch.should.receive(:in)
       @punch.in
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:in).with(@project, anything)
+      Punch.should.receive(:in) do |proj, _|
+        proj.should == @project
+      end
       @punch.in
     end
     
     it 'should pass the options when delegating to the class' do
       options = { :time => Time.now }
-      Punch.expects(:in).with(anything, options)
+      Punch.should.receive(:in) do |_, opts|
+        opts.should == options
+      end
       @punch.in(options)
     end
     
     it 'should pass an empty hash if no options given' do
-      Punch.expects(:in).with(anything, {})
+      Punch.should.receive(:in) do |_, opts|
+        opts.should == {}
+      end
       @punch.in
     end
     
@@ -140,41 +146,47 @@ describe Punch, 'instance' do
   end
   
   it 'should punch the project out' do
-    @punch.should respond_to(:out)
+    @punch.should.respond_to(:out)
   end
   
   describe 'punching the project out' do
-    before :each do
+    before do
       @out = 'out val'
-      Punch.stubs(:out).returns(@out)
+      Punch.stub!(:out).and_return(@out)
     end
     
     it 'should accept options' do
-      lambda { @punch.out(:time => Time.now) }.should_not raise_error(ArgumentError)
+      lambda { @punch.out(:time => Time.now) }.should.not.raise(ArgumentError)
     end
     
     it 'should not require options' do
-      lambda { @punch.out }.should_not raise_error(ArgumentError)
+      lambda { @punch.out }.should.not.raise(ArgumentError)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:out)
+      Punch.should.receive(:out)
       @punch.out
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:out).with(@project, anything)
+      Punch.should.receive(:out) do |proj, _|
+        proj.should == @project
+      end
       @punch.out
     end
     
     it 'should pass the options when delegating to the class' do
       options = { :time => Time.now }
-      Punch.expects(:out).with(anything, options)
+      Punch.should.receive(:out) do |_, opts|
+        opts.should == options
+      end
       @punch.out(options)
     end
     
     it 'should pass an empty hash if no options given' do
-      Punch.expects(:out).with(anything, {})
+      Punch.should.receive(:out) do |_, opts|
+        opts.should == {}
+      end
       @punch.out
     end
     
@@ -184,41 +196,47 @@ describe Punch, 'instance' do
   end
   
   it 'should list the project data' do
-    @punch.should respond_to(:list)
+    @punch.should.respond_to(:list)
   end
   
   describe 'listing the project data' do
-    before :each do
+    before do
       @list = 'list val'
-      Punch.stubs(:list).returns(@list)
+      Punch.stub!(:list).and_return(@list)
     end
     
     it 'should accept options' do
-      lambda { @punch.list(:time => Time.now) }.should_not raise_error(ArgumentError)
+      lambda { @punch.list(:time => Time.now) }.should.not.raise(ArgumentError)
     end
     
     it 'should not require options' do
-      lambda { @punch.list }.should_not raise_error(ArgumentError)
+      lambda { @punch.list }.should.not.raise(ArgumentError)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:list)
+      Punch.should.receive(:list)
       @punch.list
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:list).with(@project, anything)
+      Punch.should.receive(:list) do |proj, _|
+        proj.should == @project
+      end
       @punch.list
     end
     
     it 'should pass the options when delegating to the class' do
       options = { :time => Time.now }
-      Punch.expects(:list).with(anything, options)
+      Punch.should.receive(:list) do |_, opts|
+        opts.should == options
+      end
       @punch.list(options)
     end
     
     it 'should pass an empty hash if no options given' do
-      Punch.expects(:list).with(anything, {})
+      Punch.should.receive(:list) do |_, opts|
+        opts.should == {}
+      end
       @punch.list
     end
     
@@ -228,41 +246,47 @@ describe Punch, 'instance' do
   end
   
   it 'should get the project total' do
-    @punch.should respond_to(:total)
+    @punch.should.respond_to(:total)
   end
   
   describe 'getting the project total' do
-    before :each do
+    before do
       @total = 'total val'
-      Punch.stubs(:total).returns(@total)
+      Punch.stub!(:total).and_return(@total)
     end
     
     it 'should accept options' do
-      lambda { @punch.total(:time => Time.now) }.should_not raise_error(ArgumentError)
+      lambda { @punch.total(:time => Time.now) }.should.not.raise(ArgumentError)
     end
     
     it 'should not require options' do
-      lambda { @punch.total }.should_not raise_error(ArgumentError)
+      lambda { @punch.total }.should.not.raise(ArgumentError)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:total)
+      Punch.should.receive(:total)
       @punch.total
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:total).with(@project, anything)
+      Punch.should.receive(:total) do |proj, _|
+        proj.should == @project
+      end
       @punch.total
     end
     
     it 'should pass the options when delegating to the class' do
       options = { :time => Time.now }
-      Punch.expects(:total).with(anything, options)
+      Punch.should.receive(:total) do |_, opts|
+        opts.should == options
+      end
       @punch.total(options)
     end
     
     it 'should pass an empty hash if no options given' do
-      Punch.expects(:total).with(anything, {})
+      Punch.should.receive(:total) do |_, opts|
+        opts.should == {}
+      end
       @punch.total
     end
     
@@ -272,36 +296,40 @@ describe Punch, 'instance' do
   end
   
   it 'should log information about the project' do
-    @punch.should respond_to(:log)
+    @punch.should.respond_to(:log)
   end
   
   describe 'logging information about the project' do
-    before :each do
+    before do
       @log = 'log val'
       @message = 'some log message'
-      Punch.stubs(:log).returns(@log)
+      Punch.stub!(:log).and_return(@log)
     end
     
     it 'should accept a log message' do
-      lambda { @punch.log(@message) }.should_not raise_error(ArgumentError)
+      lambda { @punch.log(@message) }.should.not.raise(ArgumentError)
     end
     
     it 'should require a log message' do
-      lambda { @punch.log }.should raise_error(ArgumentError)
+      lambda { @punch.log }.should.raise(ArgumentError)
     end
     
     it 'should delegate to the class' do
-      Punch.expects(:log)
+      Punch.should.receive(:log)
       @punch.log(@message)
     end
     
     it 'should pass the project when delegating to the class' do
-      Punch.expects(:log).with(@project, anything)
+      Punch.should.receive(:log) do |proj, _|
+        proj.should == @project
+      end
       @punch.log(@message)
     end
     
     it 'should pass the message when delegating to the class' do
-      Punch.expects(:log).with(anything, @message)
+      Punch.should.receive(:log) do |_, msg|
+        msg.should == @message
+      end
       @punch.log(@message)
     end
     
