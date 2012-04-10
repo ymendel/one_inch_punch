d7e972967560aaacf674b8638dd755d734b709d2
Yossef Mendelssohn <ymendel@pobox.com>
2012-04-10 18:51:02 -0400

    Adding Punch.entry for making a whole time entry
    Punching in and out in one command! For your pleasure!

--- lib/punch.rb
+++ lib/punch.rb
@@ -108,7 +108,18 @@ class Punch
       end
       true
     end
-    
+
+    def entry(project, options = {})
+      raise ArgumentError, 'both :from and :to time are needed' unless options[:from] and options[:to]
+
+      in_options = { :time => options[:from] }
+      in_options[:message] = options[:message] if options[:message]
+      result = self.in(project, in_options)
+      return result unless result
+
+      out(project, :time => options[:to])
+    end
+
     def delete(project)
       return nil unless data.delete(project)
       true
