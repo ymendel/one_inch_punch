7998d7fe00c78f530e72b728f24eb9d734a1a809
Yossef Mendelssohn <ymendel@pobox.com>
2019-03-01 14:07:33 -0500

    extending Integer instead of Fixnum
    Fixnum is deprecated in modern Ruby
    
    Really I shouldn't be extending something core, but I'm too lazy to make
    a bigger change.

--- lib/punch/core_ext/integer.rb
+++ lib/punch/core_ext/integer.rb
@@ -1,11 +1,11 @@
-class Fixnum
+class Integer
   def elapsed_time
     minutes, seconds = divmod(60)
     hours, minutes   = minutes.divmod(60)
-    
+
     seconds = '%02d' % seconds
     minutes = '%02d' % minutes
-    
+
     time = "#{minutes}:#{seconds}"
     time = "#{hours}:#{time}" unless hours.zero?
     time
